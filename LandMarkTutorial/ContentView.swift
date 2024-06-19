831aefdaf307a81635955e6e7a56b472a79e6ac4
AnTran <cao.an99@gmail.com>
2024-06-19 15:20:07 +0700

    Initial Commit

---
+++ LandMarkTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandMarkTutorial
+//
+//  Created by Trần Ân on 19/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandMarkTutorial/LandMarkTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkTutorialApp.swift
+//  LandMarkTutorial
+//
+//  Created by Trần Ân on 19/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
